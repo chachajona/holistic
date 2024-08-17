78a2c598bf064c7a29b09f6e4a6bdec9081d2c4e
Dung Dang <58243954+chachajona@users.noreply.github.com>
2024-08-17 04:52:30 +0000

    init

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    experimental: { esmExternals: true },
+  };
+  
+  module.exports = nextConfig;
---
+++ tailwind.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["var(--font-roboto)", ...fontFamily.roboto],
+        robotoSerif: ["var(--font-serif)", ...fontFamily.serif],
+        robotoMono: ["var(--font-mono)", ...fontFamily.mono],
+        robotoSlab: ["var(--font-slab)", ...fontFamily.slab],
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
