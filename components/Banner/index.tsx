7a1625f43e09610aea50e1a72730ac103c6a37e7
Dung Dang <58243954+chachajona@users.noreply.github.com>
2024-08-18 18:20:10 +0000

    add Banner

---
+++ components/Banner/index.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Image from "next/image";
+import { Phone, MapPin } from "lucide-react";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import logo from "@/assets/images/Logo_Opacity_Light.png";
+
+const Banner = () => {
+  return (
+    <div className="bg-primary-background py-2">
+      <div className="container mx-auto flex items-center justify-between px-5 md:px-16">
+        <Image
+          src={logo}
+          alt="Holistic"
+          width={50}
+          height={50}
+          className="hidden p-1 md:block"
+        />
+        <div className="flex flex-col text-left font-robotoSlab font-normal lg:flex-row-reverse lg:items-center lg:gap-5">
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-primary-text hover:text-primary-hover flex items-center font-robotoSlab text-sm font-semibold"
+          >
+            <MapPin size={16} className="mr-2 hidden md:block" />
+            <span>
+              Địa chỉ: 70 Bà Huyện Thanh Quan, Phường Võ Thị Sáu, Quận 3
+            </span>
+          </a>
+          <a
+            href="tel:0828959598"
+            className="text-primary-text hover:text-primary-hover flex items-center text-sm"
+          >
+            <Phone size={16} className="mr-2 hidden md:block" />
+            <span>Hotline: 0828959598</span>
+          </a>
+          <div className="mb-1 mt-2 flex gap-4 md:hidden">
+            <a
+              href="https://www.facebook.com/holisticrep/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-primary-text hover:text-primary-hover"
+            >
+              <FaFacebook size={16} />
+            </a>
+            <a
+              href="https://www.instagram.com/hawthorneskinandbeauty/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-primary-text hover:text-primary-hover"
+            >
+              <FaInstagram size={16} />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,23 @@
 import type { Config } from "tailwindcss";
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         roboto: ["var(--font-roboto)", ...fontFamily.sans],
@@ -15,13 +25,79 @@ const config: Config = {
         robotoMono: ["var(--font-mono)", ...fontFamily.mono],
         robotoSlab: ["var(--font-slab)", ...fontFamily.sans],
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        brown: {
+          50: "#E9E4E2",
+          100: "#DED6D4",
+          200: "#C8BBB7",
+          300: "#B1A09A",
+          400: "#9B857D",
+          500: "#90776E",
+          600: "#826B63",
+          700: "#65534D",
+          800: "#483C37",
+          900: "#483C37",
+          950: "#1D1816",
+        },
+        "primary-text": "#744D40",
+        "primary-hover": "#2B2321",
+        "primary-background": "#DDD5D1",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
+
 export default config;
