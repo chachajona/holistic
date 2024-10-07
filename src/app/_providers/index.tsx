953a1ab52cb41c6227d06277713ec463f2d3593b
Dung Dang <58243954+chachajona@users.noreply.github.com>
2024-10-07 16:35:51 +0000

    init Payload

---
+++ src/app/_providers/index.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import React from 'react'
+
+import { AuthProvider } from './Auth'
+import { CartProvider } from './Cart'
+import { FilterProvider } from './Filter'
+import { ThemeProvider } from './Theme'
+
+export const Providers: React.FC<{
+  children: React.ReactNode
+}> = ({ children }) => {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <FilterProvider>
+          <CartProvider>{children}</CartProvider>
+        </FilterProvider>
+      </AuthProvider>
+    </ThemeProvider>
+  )
+}
