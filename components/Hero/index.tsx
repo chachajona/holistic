0afa5a7ac590511b1d90d8f20f5d0b3a308dd72d
Dung Dang <58243954+chachajona@users.noreply.github.com>
2024-10-24 19:20:19 +0000

    Fix bug (Hero)

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -73,7 +73,7 @@ const Hero: React.FC<HeroProps> = ({ formData }) => {
     }
 
     return (
-        <section className="bg-hero-pattern relative max-h-screen min-h-[calc(100dvh-92px)] w-full bg-cover bg-center py-16 md:min-h-[calc(100dvh-250px)] md:py-[85px]">
+        <section className="bg-hero-pattern relative max-h-screen min-h-[calc(100dvh-92px)] w-full overflow-y-auto bg-cover bg-center py-16 md:min-h-[calc(100dvh-250px)] md:py-[85px]">
             <div className="absolute inset-0 bg-black/60"></div>
             <div className="container relative md:px-16">
                 <div className="grid gap-6 lg:grid-cols-[1fr_200px] lg:gap-12 xl:grid-cols-[1fr_400px]">
@@ -89,7 +89,7 @@ const Hero: React.FC<HeroProps> = ({ formData }) => {
                             />
                         </h1>
 
-                        <div className="flex flex-col gap-4 min-[400px]:flex-row">
+                        <div className="mb-4 flex flex-col gap-4 min-[400px]:flex-row">
                             <div className="bg-primary-background/80 flex w-full flex-col space-y-3 rounded-xl p-6 shadow-lg backdrop-blur-sm transition-all duration-300 hover:shadow-xl md:max-w-md lg:max-w-lg">
                                 <h3 className="font-robotoSerif text-primary-text text-lg font-bold uppercase md:text-2xl">
                                     Tư vấn ngay trong 30 giây
@@ -107,40 +107,42 @@ const Hero: React.FC<HeroProps> = ({ formData }) => {
                                 <Form {...form}>
                                     <form
                                         onSubmit={form.handleSubmit(onSubmit)}
-                                        className="flex grow items-center space-x-2"
+                                        className="space-y-2"
                                     >
-                                        <FormField
-                                            control={form.control}
-                                            name="phoneNumber"
-                                            render={({ field }) => (
-                                                <FormItem className="relative grow">
-                                                    <FormControl>
-                                                        <Input
-                                                            type="tel"
-                                                            placeholder={
-                                                                formData
-                                                                    ?.contactFields
-                                                                    ?.phonePlaceholder ||
-                                                                "Enter your phone number"
-                                                            }
-                                                            className="bg-brown-50/70 font-robotoSlab text-primary-text caret-primary-text focus:border-primary-text focus:bg-brown-50 focus-visible:ring-primary-text w-full border-gray-300 px-5 py-2 outline-none transition-all focus-visible:ring-1 focus-visible:ring-offset-0"
-                                                            {...field}
-                                                        />
-                                                    </FormControl>
-                                                    <FormMessage />
-                                                </FormItem>
-                                            )}
-                                        />
-                                        <Button
-                                            type="submit"
-                                            className="animate-shimmer border-primary-text font-robotoSerif focus:ring-primary flex items-center justify-center space-x-0 rounded-md border bg-[linear-gradient(110deg,#744D40,45%,#8a5d4d,55%,#744D40)] bg-[length:200%_100%] px-4 py-3 font-medium text-white transition-colors hover:shadow-md focus:outline-none focus:ring-2 focus:ring-[#5a3b31] focus:ring-offset-2 focus:ring-offset-slate-50 sm:space-x-2 sm:px-6"
-                                        >
-                                            <span className="hidden sm:block">
-                                                {formData?.submitButtonText ||
-                                                    "Submit"}
-                                            </span>
-                                            <Send size={20} />
-                                        </Button>
+                                        <div className="flex items-start space-x-2">
+                                            <FormField
+                                                control={form.control}
+                                                name="phoneNumber"
+                                                render={({ field }) => (
+                                                    <FormItem className="relative grow">
+                                                        <FormControl>
+                                                            <Input
+                                                                type="tel"
+                                                                placeholder={
+                                                                    formData
+                                                                        ?.contactFields
+                                                                        ?.phonePlaceholder ||
+                                                                    "Enter your phone number"
+                                                                }
+                                                                className="bg-brown-50/70 font-robotoSlab text-primary-text caret-primary-text focus:border-primary-text focus:bg-brown-50 focus-visible:ring-primary-text w-full border-gray-300 px-5 py-2 outline-none transition-all focus-visible:ring-1 focus-visible:ring-offset-0"
+                                                                {...field}
+                                                            />
+                                                        </FormControl>
+                                                        <FormMessage className="font-robotoSlab text-sm" />
+                                                    </FormItem>
+                                                )}
+                                            />
+                                            <Button
+                                                type="submit"
+                                                className="animate-shimmer border-primary-text font-robotoSerif focus:ring-primary flex items-center justify-center space-x-0 rounded-md border bg-[linear-gradient(110deg,#744D40,45%,#8a5d4d,55%,#744D40)] bg-[length:200%_100%] px-4 py-3 font-medium text-white transition-colors hover:shadow-md focus:outline-none focus:ring-2 focus:ring-[#5a3b31] focus:ring-offset-2 focus:ring-offset-slate-50 sm:space-x-2 sm:px-6"
+                                            >
+                                                <span className="hidden sm:block">
+                                                    {formData?.submitButtonText ||
+                                                        "Submit"}
+                                                </span>
+                                                <Send size={20} />
+                                            </Button>
+                                        </div>
                                     </form>
                                 </Form>
                             </div>
